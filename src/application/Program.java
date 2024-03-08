db20434857668db76ccdbb4ff347948280fa72b5
Marco Sallas <marco.a.s.tamassia@gmail.com>
2024-03-07 22:27:12 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 	Department obj = new Department(1, "Books");
 	System.out.println(obj);
 
+	
+	Seller seller =  new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00,obj);
+	System.out.println(seller);
+	
 	}
 
 }

ff7ebb59e67ae043c400b5098161695d42ee6cde
Marco Sallas <marco.a.s.tamassia@gmail.com>
2024-03-07 22:36:39 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Seller id);
+	Seller findById(Seller id);
+	List<Seller> findAll();
+	
+}
